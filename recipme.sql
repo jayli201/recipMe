db3b935555cf151707e60009de11a7b609491162
Rebecca Zhou <rmzhou99@gmail.com>
2020-10-19 21:33:39 -0400

    Update tables and add triggers

--- recipme.sql
+++ recipme.sql
@@ -1,306 +1,360 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Oct 17, 2020 at 09:10 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `recipme`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `allergens`
---
-
-CREATE TABLE `allergens` (
-  `recipeID` int(11) NOT NULL,
-  `allergen` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `allergens`
---
-
-INSERT INTO `allergens` (`recipeID`, `allergen`) VALUES
-(1, 'peanut');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `areasofexperience`
---
-
-CREATE TABLE `areasofexperience` (
-  `username` varchar(40) NOT NULL,
-  `area` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `areasofexperience`
---
-
-INSERT INTO `areasofexperience` (`username`, `area`) VALUES
-('mds2cf', 'baking');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `categories`
---
-
-CREATE TABLE `categories` (
-  `recipeID` int(11) NOT NULL,
-  `category` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `categories`
---
-
-INSERT INTO `categories` (`recipeID`, `category`) VALUES
-(1, 'lunch');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cookpincount`
---
-
-CREATE TABLE `cookpincount` (
-  `username` varchar(40) NOT NULL,
-  `cookPinCount` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `dietaryrestrictions`
---
-
-CREATE TABLE `dietaryrestrictions` (
-  `recipeID` int(11) NOT NULL,
-  `restriction` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `dietaryrestrictions`
---
-
-INSERT INTO `dietaryrestrictions` (`recipeID`, `restriction`) VALUES
-(1, 'vegetarian');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `foodies`
---
-
-CREATE TABLE `foodies` (
-  `username` varchar(40) NOT NULL,
-  `favoriteFood` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `foodies`
---
-
-INSERT INTO `foodies` (`username`, `favoriteFood`) VALUES
-('alicehan', 'egg tarts'),
-('jasminli', 'pineapple buns'),
-('monicasandoval', 'tacos'),
-('rebeccazhou', 'fried eggs and tomatoes');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ingredients`
---
-
-CREATE TABLE `ingredients` (
-  `recipeID` int(11) NOT NULL,
-  `ingredient` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `ingredients`
---
-
-INSERT INTO `ingredients` (`recipeID`, `ingredient`) VALUES
-(1, 'bread'),
-(1, 'jelly'),
-(1, 'peanut butter');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pin`
---
-
-CREATE TABLE `pin` (
-  `recipeID` int(11) NOT NULL,
-  `username` varchar(40) NOT NULL,
-  `attempted` bit(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `pin`
---
-
-INSERT INTO `pin` (`recipeID`, `username`, `attempted`) VALUES
-(1, 'mds2cf', b'0'),
-(1, 'rmz6yx', b'1');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `recipes`
---
-
-CREATE TABLE `recipes` (
-  `recipeID` int(11) NOT NULL,
-  `username` varchar(40) NOT NULL,
-  `recipeName` varchar(40) NOT NULL,
-  `instructions` text NOT NULL,
-  `instructionCount` int(11) NOT NULL,
-  `country` varchar(40) NOT NULL,
-  `cookingTime` int(11) NOT NULL,
-  `recipePinCount` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `reviews`
---
-
-CREATE TABLE `reviews` (
-  `recipeID` int(11) NOT NULL,
-  `reviews` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `reviews`
---
-
-INSERT INTO `reviews` (`recipeID`, `reviews`) VALUES
-(1, 'This is a mediocre PB&J recipe, rip my tastebuds'),
-(1, 'Wow this is a really great PB&J recipe, 10/10 would recommend, a delectable snack');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `username` varchar(40) NOT NULL,
-  `email` varchar(40) NOT NULL,
-  `password` varchar(40) NOT NULL,
-  `firstName` varchar(40) NOT NULL,
-  `lastName` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`username`, `email`, `password`, `firstName`, `lastName`) VALUES
-('alicehan', 'alicehan@gmail.com', 'dataBase123$', 'Alice', 'Han'),
-('amh2sd', 'amh2sd@virginia.edu', 'abcD!123', 'Alice', 'Han'),
-('jasminli', 'jasminli@gmail.com', 'pa$Sword123', 'Jay', 'Li'),
-('jl6ww', 'jl6ww@virginia.edu', 'Password$', 'Jay', 'Li'),
-('mds2cf', 'mds2cf@virginia.edu', '123%Pass', 'Monica', 'Sandoval-Vasquez'),
-('monicasandoval', 'monicasandoval@gmail', '1#34pw56', 'Monica', 'Sandoval-Vasquez'),
-('rebeccazhou', 'rebeccazhou@gmail.com', 'cS47^40', 'Rebecca', 'Zhou'),
-('rmz6yx', 'rmz6yx@virginia.edu', 'pW@12340', 'Rebecca', 'Zhou');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `allergens`
---
-ALTER TABLE `allergens`
-  ADD PRIMARY KEY (`recipeID`,`allergen`);
-
---
--- Indexes for table `areasofexperience`
---
-ALTER TABLE `areasofexperience`
-  ADD PRIMARY KEY (`username`,`area`);
-
---
--- Indexes for table `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`recipeID`,`category`);
-
---
--- Indexes for table `cookpincount`
---
-ALTER TABLE `cookpincount`
-  ADD PRIMARY KEY (`username`);
-
---
--- Indexes for table `dietaryrestrictions`
---
-ALTER TABLE `dietaryrestrictions`
-  ADD PRIMARY KEY (`recipeID`,`restriction`);
-
---
--- Indexes for table `foodies`
---
-ALTER TABLE `foodies`
-  ADD PRIMARY KEY (`username`);
-
---
--- Indexes for table `ingredients`
---
-ALTER TABLE `ingredients`
-  ADD PRIMARY KEY (`recipeID`,`ingredient`);
-
---
--- Indexes for table `pin`
---
-ALTER TABLE `pin`
-  ADD PRIMARY KEY (`recipeID`,`username`);
-
---
--- Indexes for table `recipes`
---
-ALTER TABLE `recipes`
-  ADD PRIMARY KEY (`recipeID`,`username`),
-  ADD UNIQUE KEY `instructions` (`instructions`) USING HASH;
-
---
--- Indexes for table `reviews`
---
-ALTER TABLE `reviews`
-  ADD PRIMARY KEY (`recipeID`,`reviews`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`username`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+# Create tables
+# Retrieve data
+# Add data
+# Update data
+# Delete data
+
+# Create table: recipes
+CREATE TABLE IF NOT EXISTS recipes (
+    recipeID int,
+    username varchar(40),
+    recipeName varchar(40) NOT NULL,
+    instructions varchar(255) UNIQUE NOT NULL,
+    instructionCount int NOT NULL,
+    country varchar(40) NOT NULL,
+    cookingTime int NOT NULL,
+    recipePinCount int NOT NULL,
+    primary key(recipeID, username)
+);
+
+# Check Constraint
+ALTER TABLE recipes
+ADD CONSTRAINT checkInstructionCount
+CHECK (instructionCount > 0);
+
+ALTER TABLE recipes
+ADD CONSTRAINT checkCookingTime
+CHECK (cookingTime > 0);
+
+ALTER TABLE recipes
+ADD CONSTRAINT checkRecipePinCount
+CHECK (recipePinCount = 1);
+
+# Add data: recipes
+INSERT INTO recipes
+VALUES (1, 'jl6ww', 'PB&J', "Spread the peanut butter on one piece of bread. Spread the jelly on the same side. Put the two pieces of bread together to form a sandwich.", 3, 'United States', 2, 1);
+
+# Select data: recipes
+SELECT * FROM recipes
+WHERE recipes.recipeID = 1;
+
+# Update data: recipes
+UPDATE recipes SET instructions = 'Spread the peanut butter on one piece of bread. Spread the jelly on the other side. Put the two pieces of bread together to form a sandwich.' WHERE recipes.recipeID = 1 AND recipes.username = 'jl6ww';
+
+# Delete data: recipes
+DELETE FROM recipes
+WHERE recipes.recipeID = 1;
+
+# ----------------------------------
+
+# Create table: instructions
+CREATE TABLE IF NOT EXISTS instructions (
+    recipeID int,
+    instructions varchar(255),
+    difficulty varchar(10),
+    primary key(recipeID)
+);
+
+# Create trigger: instructionDifficulty
+# When a cook submits a recipe, we determine the recipe difficulty and insert the difficulty into the “instructions” table.
+DELIMITER $$
+CREATE TRIGGER instructionDifficulty
+BEFORE INSERT ON recipes
+FOR EACH ROW
+BEGIN
+	INSERT INTO instructions SET recipeID = NEW.recipeID;
+	UPDATE instructions SET instructions = NEW.instructions WHERE recipeID = NEW.recipeID;
+    	IF NEW.instructionCount BETWEEN 0 AND 5
+	THEN
+		UPDATE instructions SET difficulty = "easy" WHERE instructions = NEW.instructions;
+	ELSEIF NEW.instructionCount BETWEEN 5 AND 15
+	THEN
+		UPDATE instructions SET difficulty = "medium" WHERE instructions = NEW.instructions;
+	ELSEIF NEW.instructionCount > 15
+	THEN
+		UPDATE instructions SET difficulty = "hard" WHERE instructions = NEW.instructions;
+	END IF;
+END
+$$
+DELIMITER ;
+
+# ----------------------------------
+
+# Create table: recipePinCount
+CREATE TABLE IF NOT EXISTS recipePinCount (
+    recipeID int,
+    recipePinCount int,
+    popularity varchar(40),
+    primary key(recipeID)
+);
+
+# Create trigger: insertRecipePopularityTrigger
+# When a cook submits a recipe, it is automatically pinned by the cook so we have to update the recipe popularity. We insert a recipePinCount of 1 once the cook submits the recipe.
+DELIMITER $$
+CREATE TRIGGER insertRecipePopularityTrigger
+BEFORE INSERT ON recipes
+FOR EACH ROW
+BEGIN
+	INSERT INTO recipePinCount SET recipeID = NEW.recipeID;
+	UPDATE recipePinCount SET recipePinCount = NEW.recipePinCount WHERE recipeID = NEW.recipeID;
+    	UPDATE recipePinCount SET popularity = "up and coming" WHERE recipeID = NEW.recipeID;
+END
+$$
+DELIMITER ;
+
+# Create trigger: insertPinTrigger
+# When a cook submits a recipe, it is automatically pinned by the cook so we have to insert into the “pin” table. The “attempted” attribute is automatically set as true (1).
+
+DELIMITER $$
+CREATE TRIGGER insertPinTrigger
+BEFORE INSERT ON recipes
+FOR EACH ROW
+BEGIN
+	INSERT INTO pin SET recipeID = NEW.recipeID;
+	UPDATE pin SET username = NEW.username WHERE recipeID = NEW.recipeID;
+	UPDATE pin SET attempted = 1;
+END
+$$
+DELIMITER ;
+
+/*
+# Create trigger: updateRecipePopularityTrigger
+DELIMITER $$
+CREATE TRIGGER updateRecipePopularityTrigger
+BEFORE INSERT ON pin
+FOR EACH ROW
+BEGIN
+	UPDATE recipePinCount SET recipePinCount = NEW.recipePinCount + 1;
+    	IF NEW.recipePinCount BETWEEN 0 AND 5
+	THEN
+		UPDATE recipePinCount SET popularity = "up and coming" WHERE recipePinCount = NEW.recipePinCount;
+	ELSEIF NEW.recipePinCount BETWEEN 5 AND 15
+	THEN
+		UPDATE recipePinCount SET popularity = "rising star" WHERE recipePinCount = NEW.recipePinCount;
+	ELSEIF NEW.recipePinCount > 15
+	THEN
+		UPDATE recipePinCount SET popularity = "big hit" WHERE recipePinCount = NEW.recipePinCount;
+	END IF;
+END
+$$
+DELIMITER ;
+*/
+
+# ----------------------------------
+
+# Create table: ingredients
+CREATE TABLE IF NOT EXISTS ingredients (
+    recipeID int,
+    ingredient varchar(40),
+    primary key(recipeID, ingredient)
+);
+
+# Add data: ingredients
+INSERT INTO ingredients
+VALUES (1, 'peanut butter');
+
+INSERT INTO ingredients
+VALUES (1, 'jelly');
+
+INSERT INTO ingredients
+VALUES (1, 'bread');
+
+# ----------------------------------
+
+# Create table: categories
+CREATE TABLE IF NOT EXISTS categories (
+    recipeID int,
+    category varchar(40),
+    primary key(recipeID, category)
+);
+
+# Add data: categories
+INSERT INTO categories
+VALUES (1, 'lunch');
+
+# ----------------------------------
+
+# Create table: allergens
+CREATE TABLE IF NOT EXISTS allergens (
+    recipeID int,
+    allergen varchar(40),
+    primary key(recipeID, allergen)
+);
+
+# Add data: allergens
+INSERT INTO allergens
+VALUES (1, 'peanut');
+
+# ----------------------------------
+
+# Create table: reviews
+CREATE TABLE IF NOT EXISTS reviews (
+    recipeID int,
+    reviews varchar(255),
+    primary key(recipeID, reviews)
+);
+
+# Add data: reviews
+INSERT INTO reviews
+VALUES (1, 'Wow this is a really great PB&J recipe, 10/10 would recommend, a delectable snack');
+
+INSERT INTO reviews
+VALUES (1, 'This is a mediocre PB&J recipe, rip my tastebuds');
+
+# ----------------------------------
+
+# Create table: dietaryRestrictions
+CREATE TABLE IF NOT EXISTS dietaryRestrictions (
+    recipeID int,
+    restriction varchar(255),
+    primary key(recipeID, restriction)
+);
+
+# Add data: dietaryRestrictions
+INSERT INTO dietaryRestrictions
+VALUES (1, 'vegetarian');
+
+# ----------------------------------
+
+# Create table: pin
+CREATE TABLE IF NOT EXISTS pin (
+    recipeID int,
+    username varchar(40),
+    attempted BIT NOT NULL,
+    primary key(recipeID, username)
+);
+
+# Add data: pin
+INSERT INTO pin
+VALUES (1, 'mds2cf', 0);
+
+INSERT INTO pin
+VALUES (1, 'rmz6yx', 1);
+
+# ----------------------------------
+
+# Create table: users
+CREATE TABLE IF NOT EXISTS users (
+    username varchar(40),
+    email varchar(40) NOT NULL,
+    password varchar(40) NOT NULL,
+    firstName varchar(40) NOT NULL,
+    lastName varchar(40) NOT NULL,
+    isCook BIT NOT NULL,
+    primary key(username)
+);
+
+/*
+# Check Constraint
+ALTER TABLE users
+ADD CONSTRAINT checkPasswordLength
+CHECK (len(password) >= 8);
+
+check password like '%[0-9]%' and password like '%[A-Z]%' and password like '%[!@#$%a^&*()-_+=.,;:'"`~]%' and len(password) >= 8
+*/
+
+# Add data: users
+INSERT INTO users
+VALUES ('jl6ww', 'jl6ww@virginia.edu', 'Password$' ,'Jay', 'Li', 1);
+
+INSERT INTO users
+VALUES ('mds2cf', 'mds2cf@virginia.edu', '123%Pass' ,'Monica', 'Sandoval-Vasquez', 1);
+
+INSERT INTO users
+VALUES ('amh2sd', 'amh2sd@virginia.edu', 'abcD!123' , 'Alice', 'Han', 1);
+
+INSERT INTO users
+VALUES ('rmz6yx', 'rmz6yx@virginia.edu', 'pW@12340' ,'Rebecca', 'Zhou', 1);
+
+INSERT INTO users
+VALUES ('jasminli', 'jasminli@gmail.com', 'pa$Sword123' ,'Jay', 'Li', 0);
+
+INSERT INTO users
+VALUES ('monicasandoval', 'monicasandoval@gmail', '1#34pw56' ,'Monica', 'Sandoval-Vasquez', 0);
+
+INSERT INTO users
+VALUES ('alicehan', 'alicehan@gmail.com', 'dataBase123$' , 'Alice', 'Han', 0);
+
+INSERT INTO users
+VALUES ('rebeccazhou', 'rebeccazhou@gmail.com', 'cS47^40' ,'Rebecca', 'Zhou', 0);
+
+# ----------------------------------
+
+# Create table: foodies
+CREATE TABLE IF NOT EXISTS foodies (
+    username varchar(40),
+    favoriteFood varchar(40) NOT NULL,
+    primary key(username)
+);
+
+# Add data: foodies
+INSERT INTO foodies
+VALUES ('jasminli', 'pineapple buns');
+
+INSERT INTO foodies
+VALUES ('monicasandoval', 'tacos');
+
+INSERT INTO foodies
+VALUES ('alicehan', 'egg tarts');
+
+INSERT INTO foodies
+VALUES ('rebeccazhou', 'fried eggs and tomatoes');
+
+# ----------------------------------
+
+# Create table: cookPinCount
+CREATE TABLE IF NOT EXISTS cookPinCount(
+    username varchar(40),
+    cookPinCount int NOT NULL,
+    expertise varchar(40),
+    primary key(username)
+);
+
+# Create trigger: insertCookExpertiseTrigger
+# When a cook account is created, we insert into the “expertise” table to indicate an expertise of “amateur cook”. We insert a cookPinCount of 0 once the cook is created.
+DELIMITER $$
+CREATE TRIGGER insertCookExpertiseTrigger
+BEFORE INSERT ON users
+FOR EACH ROW
+BEGIN
+	IF NEW.isCook = 1
+	THEN
+	INSERT INTO cookPinCount SET username = NEW.username;
+	UPDATE cookPinCount SET cookPinCount = 0;
+    	UPDATE cookPinCount SET expertise = "amateur cook" WHERE username = NEW.username;
+	END IF;
+END
+$$
+DELIMITER ;
+
+# ----------------------------------
+
+# Create table: cookPinCount
+CREATE TABLE IF NOT EXISTS cookPinCount (
+    username varchar(40),
+    cookPinCount int NOT NULL,
+    primary key(username)
+);
+
+# ----------------------------------
+
+# Create table: areasOfExperience
+
+CREATE TABLE IF NOT EXISTS areasOfExperience (
+    username varchar(40),
+    area varchar(40),
+    primary key(username, area)
+);
+ 
+# Add data: areasOfExperience
+INSERT INTO areasOfExperience
+VALUES ('mds2cf', 'baking');
+
+INSERT INTO areasOfExperience
+VALUES ('jl6ww', 'baking');
+
+INSERT INTO areasOfExperience
+VALUES ('rmz6yx', 'noodling');
+
+INSERT INTO areasOfExperience
+VALUES ('amh2sd', 'microwaving');
